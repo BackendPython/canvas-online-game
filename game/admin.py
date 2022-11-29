4e5f4d29b5404cb59e7632feb53330771dd5128c
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-29 15:41:54 +0500

    new functions

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from game.models import *
 
-# Register your models here.
+admin.site.register(CustomeUser)
+admin.site.register(PlayerModel)
--- game/models.py
+++ game/models.py
@@ -6,3 +6,14 @@ class CustomeUser(AbstractUser):
     victory_battles = models.IntegerField(default=0)
     defeat_battles = models.IntegerField(default=0)
     
+class PlayerModel(models.Model):
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    vx = models.IntegerField(default=0)
+    vy = models.IntegerField(default=0)
+    dead = models.BooleanField(default=False)
+    remove_heal = models.IntegerField(default=0)
+    orignal_heal = models.IntegerField(default=100)
+    turn = models.CharField(max_length=20, default='idle')
+    name = models.CharField(max_length=10, default='null')
+    
