5baca59f1136509b5ba221831b01809320e009b3
backendpython <holmirzaevsohruh196@gmail.com>
2022-11-29 14:07:47 +0500

    new functions

--- game/models.py
+++ game/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from  django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomeUser(AbstractUser):
+    all_battle = models.IntegerField(default=0)
+    victory_battles = models.IntegerField(default=0)
+    defeat_battles = models.IntegerField(default=0)
+    
